3ad1e20d1efa07cff9b68895430a388d68ee2cef
AlexisCastaneda <A01750119@itesm.mx>
2021-03-19 07:44:18 -0600

    Cambio en el archivo ramaDOs

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -5,3 +5,10 @@ import numpy as np
 IRGB=cv2.imread('venom.jpg')
 print(IRGB)
 print(IRGB.shape)
+
+
+print("Lineas agregadas en la ramaDos")
+IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS)
+print(IGS.shape)
+
